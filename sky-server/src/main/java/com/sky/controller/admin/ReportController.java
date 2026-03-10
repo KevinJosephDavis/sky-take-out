6891cd42f5b995dc5a8ff07b7fb46f23c1ee8a60
tkevinjd <3160494497@qq.com>
2026-03-10 23:13:37 +0800

    feat:新增工作台、导出运营数据报表功能

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -78,5 +79,15 @@ public class ReportController {
                                             @DateTimeFormat (pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("查询日期区间{}到{}的销量排名Top10", begin, end);
         return Result.success(reportService.getgSalesTop10(begin, end));
+    }
+
+    /**
+     * 数据导出
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出数据")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -59,4 +60,11 @@ public interface DishMapper {
      * 根据分类id查询菜品（已启用）数据
      */
     List<Dish> list(Dish dish);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
